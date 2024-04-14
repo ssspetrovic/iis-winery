f80362c0613b063823698d016d5625003418f3bc
Srdjan Petrovic <s.petrovic852@gmail.com>
2024-04-14 18:09:11 +0200

    Reformat home page

--- frontend/winery/src/App.jsx
+++ frontend/winery/src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/auth/Register";
 import Home from "./components/Home";
@@ -16,37 +15,46 @@ import RequireAuth from "./components/auth/RequireAuth";
 import WinemakerOrdersPage from "./components/orders/WinemakerOrderPage";
 import Unauthorized from "./components/auth/Unauthorized";
 import NotFound from "./components/util/NotFound";
+import MainNavbar from "./components/util/MainNavbar";
 import { ROLES } from "./components/auth/Roles";
+import "./index.css";
+import "./assets/styles.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        {/* Unprotected */}
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/unauthorized" element={<Unauthorized />} />
+    <>
+      <MainNavbar />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          {/* Unprotected */}
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/unauthorized" element={<Unauthorized />} />
 
-        {/* Private */}
-        <Route element={<RequireAuth allowedRoles={ROLES.ADMIN} />}>
-          <Route path="/admin-profile/:username" element={<AdminProfile />} />
+          {/* Private */}
+          <Route element={<RequireAuth allowedRoles={ROLES.ADMIN} />}>
+            <Route path="/admin-profile/:username" element={<AdminProfile />} />
+          </Route>
+
+          {/* TODO: Update roles accordingly (you can import constant role dictionary from Roles.jsx) */}
+          <Route path="/view-users" element={<ViewUsers />} />
+          <Route path="/register-worker" element={<RegisterWorker />} />
+          <Route path="/update-worker" element={<UpdateWorker />} />
+          <Route path="/view-vehicles" element={<VehiclesList />} />
+          <Route path="/add-vehicle" element={<AddVehicle />} />
+          <Route path="/update-vehicle" element={<UpdateVehicle />} />
+          <Route path="/view-reports" element={<ReportList />} />
+          <Route
+            path="/winemaker-order-page"
+            element={<WinemakerOrdersPage />}
+          />
+
+          {/* If no other routes are hooked, throw 404 */}
+          <Route path="*" element={<NotFound />} />
         </Route>
-
-        {/* TODO: Update roles accordingly (you can import constant role dictionary from Roles.jsx) */}
-        <Route path="/view-users" element={<ViewUsers />} />
-        <Route path="/register-worker" element={<RegisterWorker />} />
-        <Route path="/update-worker" element={<UpdateWorker />} />
-        <Route path="/view-vehicles" element={<VehiclesList />} />
-        <Route path="/add-vehicle" element={<AddVehicle />} />
-        <Route path="/update-vehicle" element={<UpdateVehicle />} />
-        <Route path="/view-reports" element={<ReportList />} />
-        <Route path="/winemaker-order-page" element={<WinemakerOrdersPage />} />
-
-        {/* If no other routes are hooked, throw 404 */}
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
+      </Routes>
+    </>
   );
 }
 
--- frontend/winery/src/components/Home.jsx
+++ frontend/winery/src/components/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Button, Row, Col } from "reactstrap";
+import { Button } from "reactstrap";
 import useAuth from "../hooks/useAuth";
 import useRefreshToken from "../hooks/useRefreshToken";
 import { useAxiosPrivate } from "../hooks/useAxiosPrivate";
 import profileRedirects from "../components/users/ProfileRedirect";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Home = () => {
   const { auth, logout } = useAuth();
@@ -14,12 +14,16 @@ const Home = () => {
   const axiosPrivate = useAxiosPrivate();
   const navigate = useNavigate();
 
+  const [authStatus, setAuthStatus] = useState(false);
+
   const checkAuth = async () => {
     try {
       const response = await axiosPrivate.get("/hello-auth/");
       console.log(response);
+      setAuthStatus(true);
     } catch (error) {
       console.error(error);
+      setAuthStatus(false);
     }
   };
 
@@ -29,32 +33,76 @@ const Home = () => {
   };
 
   return (
-    <div className="div-center">
-      <h1>Hello from home page!</h1>
-      <h3>Username: {username}</h3>
-      <h3>Role: {role}</h3>
-      <h3>Access token:</h3> <p>{accessToken}</p>
-      <h3>Refresh token:</h3> <p>{refreshToken}</p>
-      <h4>
-        Register: <Link to="/register">click here</Link>
-      </h4>
-      <h4>
-        Login: <Link to="/login">click here</Link>
-      </h4>
-      <Button style={{ margin: "5px" }} onClick={() => logout()}>
-        Log out
-      </Button>
-      <Button style={{ margin: "5px" }} onClick={() => refresh()}>
-        Refresh token
-      </Button>
-      <Row style={{ margin: "10px" }}>
-        <Col>
-          <Button onClick={() => checkAuth()}>Check Authentication</Button>
-          <Button onClick={() => profilePage()}>
-            Redirect to Profile Page
-          </Button>
-        </Col>
-      </Row>
+    <div className="div-center m-4">
+      <div className="text-center my-4">
+        <h1>Hello from home page!</h1>
+      </div>
+      <div className="mx-5">
+        <h4>Username: {username ? username : "/"}</h4>
+        <h4>Role: {role ? role : "/"}</h4>
+        <h4>Access token:</h4>
+        <div>
+          <textarea
+            className="form-control w-100"
+            name="accessToken"
+            value={accessToken ? accessToken : "/"}
+            readOnly
+          />
+        </div>
+        <h4>Refresh token:</h4>
+        <div>
+          <textarea
+            className="form-control w-100"
+            name="refreshToken"
+            value={refreshToken ? refreshToken : "/"}
+            readOnly
+          />
+        </div>
+      </div>
+      <div className="container m-4 ">
+        <Button
+          className="w-100"
+          style={{ margin: "5px" }}
+          onClick={() => profilePage()}
+        >
+          Redirect to Profile Page
+        </Button>
+        <Button
+          className="w-100"
+          style={{ margin: "5px" }}
+          onClick={() => navigate("/login")}
+        >
+          Log in
+        </Button>
+        <Button
+          className="w-100"
+          style={{ margin: "5px" }}
+          onClick={() => logout()}
+        >
+          Log out
+        </Button>
+        <Button
+          className="w-100"
+          style={{ margin: "5px" }}
+          onClick={() => navigate("/register")}
+        >
+          Register
+        </Button>
+        <Button
+          className="w-100"
+          style={{ margin: "5px" }}
+          onClick={() => refresh()}
+        >
+          Refresh token
+        </Button>
+        <Button
+          className="w-100"
+          style={{ margin: "5px" }}
+          onClick={() => checkAuth()}
+        >
+          Check Authentication {authStatus ? "[SUCCESSFUL]" : "[FAILED]"}
+        </Button>
+      </div>
     </div>
   );
 };
