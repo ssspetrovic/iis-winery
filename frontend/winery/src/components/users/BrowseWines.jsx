74b91d3ad614bb8341dd8e9e3e69419928068e18
Srdjan Petrovic <s.petrovic852@gmail.com>
2024-05-06 17:38:50 +0200

    Improve display of wines, fix any in filter

--- frontend/winery/src/components/users/BrowseWines.jsx
+++ frontend/winery/src/components/users/BrowseWines.jsx
@@ -10,8 +10,26 @@ import {
   CardTitle,
   CardSubtitle,
   CardImg,
+  Form,
+  FormGroup,
+  Input,
 } from "reactstrap";
 import { useAxiosPrivate } from "../../hooks/useAxiosPrivate";
+
+import wineImage1 from "../../assets/images/wine_4x5_01.jpg";
+import wineImage2 from "../../assets/images/wine_4x5_02.jpg";
+import wineImage3 from "../../assets/images/wine_4x5_03.jpg";
+
+function getRandomImage() {
+  let rand = Math.floor(Math.random() * 3);
+  if (rand === 0) {
+    return wineImage1;
+  } else if (rand === 1) {
+    return wineImage2;
+  } else {
+    return wineImage3;
+  }
+}
 
 const BrowseWines = () => {
   const axiosPrivate = useAxiosPrivate();
@@ -73,9 +91,9 @@ const BrowseWines = () => {
                       <i className="fa fa-filter" /> Filter
                     </div>
                   </Col>
-                  <Col xs={2}>
+                  <Col xs={2} className="text-end">
                     <div
-                      className="text-end cursor-pointer"
+                      className="d-flex align-items-center justify-content-end cursor-pointer"
                       onClick={resetFilter}
                     >
                       Reset
@@ -89,8 +107,12 @@ const BrowseWines = () => {
                   </Col>
                   <Col xs={6}></Col>
                   <Col xs={2} className="text-end">
-                    <div className="cursor-pointer" onClick={handleFilter}>
-                      Any <i className="fa-solid fa-chevron-right" />
+                    <div
+                      className="d-flex align-items-center justify-content-end cursor-pointer"
+                      onClick={handleFilter}
+                    >
+                      <div className="me-2">Any</div>
+                      <i className="fa-solid fa-chevron-right" />
                     </div>
                   </Col>
                 </Row>
@@ -101,8 +123,12 @@ const BrowseWines = () => {
                   </Col>
                   <Col xs={6}></Col>
                   <Col xs={2} className="text-end">
-                    <div className="cursor-pointer" onClick={handleFilter}>
-                      Any <i className="fa-solid fa-chevron-right" />
+                    <div
+                      className="d-flex align-items-center justify-content-end cursor-pointer"
+                      onClick={handleFilter}
+                    >
+                      <div className="me-2">Any</div>
+                      <i className="fa-solid fa-chevron-right" />
                     </div>
                   </Col>
                 </Row>
@@ -113,8 +139,12 @@ const BrowseWines = () => {
                   </Col>
                   <Col xs={6}></Col>
                   <Col xs={2} className="text-end">
-                    <div className="cursor-pointer" onClick={handleFilter}>
-                      Any <i className="fa-solid fa-chevron-right" />
+                    <div
+                      className="d-flex align-items-center justify-content-end cursor-pointer"
+                      onClick={handleFilter}
+                    >
+                      <div className="me-2">Any</div>
+                      <i className="fa-solid fa-chevron-right" />
                     </div>
                   </Col>
                 </Row>
@@ -125,8 +155,12 @@ const BrowseWines = () => {
                   </Col>
                   <Col xs={6}></Col>
                   <Col xs={2} className="text-end">
-                    <div className="cursor-pointer" onClick={handleFilter}>
-                      Any <i className="fa-solid fa-chevron-right" />
+                    <div
+                      className="d-flex align-items-center justify-content-end cursor-pointer"
+                      onClick={handleFilter}
+                    >
+                      <div className="me-2">Any</div>
+                      <i className="fa-solid fa-chevron-right" />
                     </div>
                   </Col>
                 </Row>
@@ -137,8 +171,12 @@ const BrowseWines = () => {
                   </Col>
                   <Col xs={6}></Col>
                   <Col xs={2} className="text-end">
-                    <div className="cursor-pointer" onClick={handleFilter}>
-                      Any <i className="fa-solid fa-chevron-right" />
+                    <div
+                      className="d-flex align-items-center justify-content-end cursor-pointer"
+                      onClick={handleFilter}
+                    >
+                      <div className="me-2">Any</div>
+                      <i className="fa-solid fa-chevron-right" />
                     </div>
                   </Col>
                 </Row>
@@ -149,8 +187,12 @@ const BrowseWines = () => {
                   </Col>
                   <Col xs={6}></Col>
                   <Col xs={2} className="text-end">
-                    <div className="cursor-pointer" onClick={handleFilter}>
-                      Any <i className="fa-solid fa-chevron-right" />
+                    <div
+                      className="d-flex align-items-center justify-content-end cursor-pointer"
+                      onClick={handleFilter}
+                    >
+                      <div className="me-2">Any</div>
+                      <i className="fa-solid fa-chevron-right" />
                     </div>
                   </Col>
                 </Row>
@@ -163,22 +205,74 @@ const BrowseWines = () => {
             </Collapse>
           </Col>
           <Col md={9}>
-            <div>
-              {wines.map((wine) => (
-                <Card key={wine.id} className="mb-2 shadow">
-                  <CardImg top width="100%" src={wine.image} alt={wine.name} />
-                  <CardBody>
-                    <CardTitle tag="h5">{wine.name}</CardTitle>
-                    <CardSubtitle tag="h6" className="mb-2 text-muted">
-                      Sweetness: {wine.sweetness}
-                    </CardSubtitle>
-                    <p>Acidity: {wine.acidity}</p>
-                    <p>Alcohol: {wine.alcohol}</p>
-                    <p>pH: {wine.pH}</p>
-                    <p>Winemaker: {wine.winemaker}</p>
-                  </CardBody>
-                </Card>
-              ))}
+            <div className="">
+              {wines.map(
+                (wine, index) =>
+                  index % 4 === 0 && (
+                    <Row key={index}>
+                      {wines.slice(index, index + 4).map((wine) => (
+                        <Col md={3} key={wine.id} className="px-1">
+                          <Card className="mb-2 shadow">
+                            <CardImg
+                              className="card-img-top"
+                              src={getRandomImage()}
+                              width="40%"
+                              alt="wine image"
+                            />
+                            <CardBody>
+                              <Row>
+                                <Col md={10}>
+                                  <CardTitle tag="h5">{wine.name}</CardTitle>
+                                </Col>
+                                <Col md={2}>
+                                  <div className="d-flex justify-content-center align-items-center h-100">
+                                    <i className="fa fa-heart"></i>
+                                  </div>
+                                </Col>
+                              </Row>
+                              <CardSubtitle
+                                tag="h6"
+                                className="mb-2 text-muted"
+                              >
+                                <b>Sweetness:</b> {wine.sweetness}
+                              </CardSubtitle>
+                              <div>
+                                <p className="mb-1">
+                                  <b>Acidity:</b> {wine.acidity}
+                                </p>
+                                <p className="mb-1">
+                                  <b>Alcohol:</b> {wine.alcohol}
+                                </p>
+                                <p className="mb-1">
+                                  <b>pH:</b> {wine.pH}
+                                </p>
+                                <p className="mb-1">
+                                  <b>Winemaker:</b> {wine.winemaker}
+                                </p>
+                                <Row className="mt-3">
+                                  <Col md={4}>
+                                    <div className="d-flex justify-content-center align-items-center h-100">
+                                      <Form>
+                                        <Input type="number" value={1} />
+                                      </Form>
+                                    </div>
+                                  </Col>
+                                  <Col md={8}>
+                                    <div className="text-center my-auto">
+                                      <Button color="dark" className="w-100">
+                                        <small>Add to cart</small>
+                                      </Button>
+                                    </div>
+                                  </Col>
+                                </Row>
+                              </div>
+                            </CardBody>
+                          </Card>
+                        </Col>
+                      ))}
+                    </Row>
+                  )
+              )}
             </div>
           </Col>
         </Row>
