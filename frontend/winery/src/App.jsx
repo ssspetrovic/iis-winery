7fc60aeb2704060655fc282bdc1a35f57652ddb2
Srdjan Petrovic <s.petrovic852@gmail.com>
2024-05-22 16:50:17 +0200

    Reapply "Design Update and Migrations Fix"
    This reverts commit 7648de9de03e34c3cad911df605b6e5848faf1d9.

--- frontend/winery/src/App.jsx
+++ frontend/winery/src/App.jsx
@@ -23,7 +23,6 @@ import PasswordResetForm from "./components/auth/PasswordResetForm";
 import PasswordResetConfirmForm from "./components/auth/PasswordResetConfirmForm";
 import WineRoomsPage from "./components/wine_cellar/WineRoomPage";
 import OrderPage from "./components/orders/OrderPage";
-import NavigationBar from "./components/util/WinemakerNavbar";
 import BrowseWines from "./components/customers/BrowseWines";
 import ShoppingCart from "./components/customers/ShoppingCart";
 import Home from "../Home";
@@ -113,7 +112,6 @@ function App() {
     // </>
     <div>
       <MainNavbar />
-      <NavigationBar />
       <Routes>
         <Route path="/" element={<Layout />}>
           {/* Unprotected */}
